064a356cf27fd1aac55333422d5de1a140dc711b
SOYUNJALIYEV <A.SOYUNJALIYEV@bycn.bouygues-construction.com>
2023-11-17 17:16:13 +0500

    Fixed errors

--- README.md
+++ README.md
@@ -1,23 +1,49 @@
 # vocal-instrumental
-Separating vocal and instrumental, with django backend and angular frontend
+Separating vocal and instrumental, with django backend and angular frontend. Spleeter library is used to do this.
 
-1 - Clone or download this repository
 
-2 - Goto downloaded folder and create virtual environment
-    ```python3 -m venv venv```
+1- This is written with python version 3.8.10. So any python of version 3.8.xx should be OK.
 
-3 - Install necessary packages
-    ```pip3 install -r requirements.txt```
 
-4 - Run the server
-    ```python3 manage.py runserver```
+2-In your system(windows, mac, linux) be sure that ```ffmpeg``` and ```libsndfile``` are 
+installed, and path of both are added to path(environmental variables). To be sure, 
+after installation write 'ffmpeg' to terminal, if there is no error then it is good to go. And to check if another one is installed correctly. To check write 'sndfile-cmp' to terminal,
+again if there is no error then you are good to go.
 
-5 - Navigate to your browser and open localhost:8000
 
-6 - After you send you mp3 file, It will save it to 'output', which is located inside your django project
+3 - Clone or download this repository
+
+4 - Goto downloaded folder, and navigate to backend folder and create virtual environment
+    ```python3 -m venv venv``` 
+    or if you are on windows 
+    ```python -m venv venv```
+
+5 - Activate vitual environment. In terminal:
+    ```source venv/bin/activate```
+    or if you are on windows 
+    ```venv/Scripts/activate```
+
+6 - Inside activated virtual environment install necessary packages
+    ```pip3 install -r requirements.txt```  
+    or if you are on windows 
+    ```pip install -r requirements.txt```
+
+7 - Run the server
+    ```python3 manage.py runserver``` 
+    or if you are on windows 
+    ```python manage.py runserver``` 
+
+8 - Navigate to your browser and open localhost:8000
+
+9 - After you send you mp3 file, it will retieve separated audio files and you can play it
+
+
+ERROR???: Spleeter has pre trained models to work with already. But they are not available locally. So when this application starts first time, spleeter automatically downloads it from remote repository for once, later it uses it. Sometimes it cannot download that, download process fails with SSL_ERROR. In this case download pre_trained models from repository, apprx 60-70mb (https://github.com/deezer/spleeter/releases/download/v1.4.0/2stems.tar.gz). Then place it under backend/pretrained_models/2stems/(downloaded files). Then try again.
 
 
 What to do next?
 
-Frontend with angular, when user uploads mp3 it will send back 2 mp3 files, which are vocals.mp3 and
-accompaniment.mp3.
+- Frontend with angular.
+- Generated audio files are too big, it has to be optimized.
+- At the moment it processes one file at once on batch, later it can cause performance issues and out of memory issues, optimization is need here by separating audio files into chunks. So that it can be memory friendly.
+- Generated files are stored in storage for the moment, it is done for development purposes. Later backend will delete it after sending it to client. This way storage will not be throttled for nothing.
--- backend/separator/views.py
+++ backend/separator/views.py
@@ -1,10 +1,12 @@
-import os
+import os, subprocess, base64
 import tempfile
+from pathlib import Path
 from django.http import HttpResponse, JsonResponse, HttpResponseServerError, FileResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from rest_framework.decorators import api_view
 from spleeter.separator import Separator
+from rest_framework.response import Response
 from pydub import AudioSegment
 import zipfile
 
@@ -24,59 +26,96 @@ def separate_view(request):
         # Path to the temporary file
         input_audio = temp_file.name
 
-        # Output directory based on your Django app's BASE_DIR
-        output_dir = os.path.join(settings.BASE_DIR, 'output')
-
         separator = Separator("spleeter:2stems")
 
+        # Output directory based on your Django app's BASE_DIR
+        output_dir = os.path.join(settings.BASE_DIR, 'output')
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
-        audio = AudioSegment.from_file(input_audio)
+        separator.separate_to_file(input_audio, output_dir)
 
-        chunk_duration = 30 * 1000
-        vocals_segments = []
-        accompaniment_segments = []
+        # BELOW CODE IS INTENDED FOR OPTIMIZATION
+        # IN CASE OF LACK OF RESOURCE IT SEPARATE FILE
+        # INTO CHUNKS FIRST, THEN DO SEPARATION WORK ONE BY ONE
+        # AT THEN END IT MERGES ALL RESULT AUDIO FILES INTO FINAL AUDIO
+        # audio = AudioSegment.from_file(input_audio)
 
-        for start_time in range(0, len(audio), chunk_duration):
-            chunk = audio[start_time:start_time + chunk_duration]
-            chunk_file = os.path.join(output_dir, f"chunk_{start_time}.mp3")
-            chunk.export(chunk_file, format="mp3")
-            separator.separate_to_file(chunk_file, output_dir)
+        # chunk_duration = 30 * 1000
+        # vocals_segments = []
+        # accompaniment_segments = []
 
-            vocal_file = os.path.join(output_dir, f"chunk_{start_time}", "vocals.wav")
-            accompaniment_file = os.path.join(output_dir, f"chunk_{start_time}", "accompaniment.wav")
+        # for start_time in range(0, len(audio), chunk_duration):
+        #     chunk = audio[start_time:start_time + chunk_duration]
+        #     chunk_file = os.path.join(output_dir, f"chunk_{start_time}.mp3")
+        #     chunk.export(chunk_file, format="mp3")
 
-            vocals = AudioSegment.from_wav(vocal_file)
-            accompaniment = AudioSegment.from_wav(accompaniment_file)
+        #     # Use Spleeter to separate vocals and accompaniment
+        #     separator.separate_to_file(chunk_file, output_dir)
+        #     # Define the command
+        #     # command = ['spleeter', 'separate', '-p', 'spleeter:2stems', '-o', output_dir, chunk_file]
+        #     # Run the command using subprocess.run
+        #     # subprocess.run(command, check=True)
+        
+        #     vocal_file = os.path.join(output_dir, f"chunk_{start_time}", "vocals.wav")
+        #     accompaniment_file = os.path.join(output_dir, f"chunk_{start_time}", "accompaniment.wav")
 
-            vocals_segments.append(vocals)
-            accompaniment_segments.append(accompaniment)
+        #     vocals = AudioSegment.from_wav(vocal_file)
+        #     accompaniment = AudioSegment.from_wav(accompaniment_file)
 
-            # os.remove(vocal_file)
-            # os.remove(accompaniment_file)
-            # os.remove(chunk_file)
+        #     vocals_segments.append(vocals)
+        #     accompaniment_segments.append(accompaniment)
 
-        merged_vocals = sum(vocals_segments)
-        merged_accompaniment = sum(accompaniment_segments)
+        #     # os.remove(vocal_file)
+        #     # os.remove(accompaniment_file)
+        #     # os.remove(chunk_file)
+
+        # merged_vocals = sum(vocals_segments)
+        # merged_accompaniment = sum(accompaniment_segments)
 
         # Export the merged audio segments to separate files
-        merged_vocals.export(os.path.join(output_dir, "vocals.mp3"), format="mp3")
-        merged_accompaniment.export(os.path.join(output_dir, "accompaniment.mp3"), format="mp3")
+        # merged_vocals.export(os.path.join(output_dir, "vocals.mp3"), format="mp3")
+        # merged_accompaniment.export(os.path.join(output_dir, "accompaniment.mp3"), format="mp3")
 
-        # Create a ZIP archive containing the separated audio files
-        zip_file_path = os.path.join(output_dir, 'separated_audio.zip')
-        with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-            zipf.write(os.path.join(output_dir, 'vocals.mp3'), 'vocals.mp3')
-            zipf.write(os.path.join(output_dir, 'accompaniment.mp3'), 'accompaniment.mp3')
+        # # Create a ZIP archive containing the separated audio files
+        # zip_file_path = os.path.join(output_dir, 'separated_audio.zip')
+        # with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        #     zipf.write(os.path.join(output_dir, 'vocals.mp3'), 'vocals.mp3')
+        #     zipf.write(os.path.join(output_dir, 'accompaniment.mp3'), 'accompaniment.mp3')
 
-        # Return the ZIP archive as a response
-        with open(zip_file_path, 'rb') as zip_file:
-            response = HttpResponse(zip_file.read(), content_type='application/zip')
-            response['Content-Disposition'] = 'attachment; filename="separated_audio.zip"'
-            return response
+        # # Return the ZIP archive as a response
+        # with open(zip_file_path, 'rb') as zip_file:
+        #     response = HttpResponse(zip_file.read(), content_type='application/zip')
+        #     response['Content-Disposition'] = 'attachment; filename="separated_audio.zip"'
+        #     return response
+
+        # Read the separated audio files
+        vocals_file = os.path.join(output_dir, Path(temp_file.name).stem, 'vocals.wav')
+        accompaniment_file = os.path.join(output_dir, Path(temp_file.name).stem, 'accompaniment.wav')
+
+        with open(vocals_file, 'rb') as vocals_file:
+            vocals_content = base64.b64encode(vocals_file.read()).decode('utf-8')
+
+        with open(accompaniment_file, 'rb') as accompaniment_file:
+            accompaniment_content = base64.b64encode(accompaniment_file.read()).decode('utf-8')
+
+
+        # Return the response as JSON
+        response_dict = {
+            'vocals': vocals_content,
+            'accompaniment': accompaniment_content,
+        }
+
+        return JsonResponse(response_dict)
     except Exception as f:
         print('general exception', str(f))
         return HttpResponseServerError('An error occurred during audio processing')
+    except subprocess.CalledProcessError as e:
+        # Handle errors, log them, and return an error response
+        error_message = f'Error executing Spleeter command: {e}'
+        # Log the error message
+        print(error_message)
+        # Return an error response
+        return HttpResponse(error_message, status=500)
     finally:
         os.remove(input_audio)
